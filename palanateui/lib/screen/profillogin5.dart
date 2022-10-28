a651a6cc10bce91dde87dfdd97a13df3fb57b92c
MD. Maruf <61429716+marufmon@users.noreply.github.com>
2022-10-28 15:49:58 +0600

    Update profillogin5.dart

--- palanateui/lib/screen/profillogin5.dart
+++ palanateui/lib/screen/profillogin5.dart
@@ -20,100 +20,101 @@ class _Login12State extends State<Login12> {
   final TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.black,
-      body: Container(
-        padding: EdgeInsets.all(10),
-        height: MediaQuery.of(context).size.height * 1.00,
-        width: double.infinity,
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                leading: IconButton(
-                    onPressed: (() {
-                      Navigator.of(context).pop();
-                    }),
-                    icon: Padding(
-                      padding: const EdgeInsets.only(left: 0, bottom: 15),
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                    )),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 70, bottom: 25),
-                child: Text(
-                  "Invite your team members",
-                  style: GoogleFonts.roboto(fontSize: 18, color: Colors.white),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.black,
+        body: Container(
+          padding: EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height * 1.00,
+          width: double.infinity,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: IconButton(
+                      onPressed: (() {
+                        Navigator.of(context).pop();
+                      }),
+                      icon: Padding(
+                        padding: const EdgeInsets.only(left: 0, bottom: 15),
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      )),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, bottom: 2),
-                child: Text(
-                  " Email members",
-                  style: GoogleFonts.roboto(fontSize: 16, color: Colors.white),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "email is empty";
-                  }
-                  if (!value.contains("@")) {
-                    return "email feild requred";
-                  }
-                },
-                controller: emailController,
-                decoration: InputDecoration(
-                    hintText: "Types an email address",
-                    labelText: "Types an email address",
-                    hintStyle: TextStyle(color: Colors.black),
-                    labelStyle: TextStyle(color: Colors.black),
-                    fillColor: Color.fromARGB(183, 158, 158, 158),
-                    filled: true,
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.green,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.black))),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 150),
-                child: CustomeButton(
-                  title: "Continue",
-                  onTap: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => Login12()));
+                Padding(
+                  padding: const EdgeInsets.only(left: 70, bottom: 25),
+                  child: Text(
+                    "Invite your team members",
+                    style:
+                        GoogleFonts.roboto(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, bottom: 2),
+                  child: Text(
+                    " Email members",
+                    style:
+                        GoogleFonts.roboto(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "email is empty";
+                    }
+                    if (!value.contains("@")) {
+                      return "email feild requred";
                     }
                   },
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      hintText: "Types an email address",
+                      labelText: "Types an email address",
+                      hintStyle: TextStyle(color: Colors.black),
+                      labelStyle: TextStyle(color: Colors.black),
+                      fillColor: Color.fromARGB(183, 158, 158, 158),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.green,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.black)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.black))),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 150),
+                  child: CustomeButton(
+                    title: "Continue",
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
