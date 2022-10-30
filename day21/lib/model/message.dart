eebc845be18d1cf96bac54d682792f61d7f3a25e
MD. Maruf <61429716+marufmon@users.noreply.github.com>
2022-10-30 19:04:32 +0600

    Ui

--- day21/lib/model/message.dart
+++ day21/lib/model/message.dart
@@ -1,13 +1,27 @@
 class Message {
-  String logoURl;
-  String title;
-  String fullTime;
   String company;
+  String title;
   String location;
+  String logoURl;
+  String fullTime;
+
   List<String> req;
-  Message(this.logoURl, this.title, this.fullTime, this.location, this.company,
+  Message(this.company, this.title, this.location, this.logoURl, this.fullTime,
       this.req);
   static List<Message> generatedMessageList() {
-    return [Message("", "", "", "", "", [])];
+    return [
+      Message("Google", "Software developer", "66/5 India", "images/m.22.png",
+          "Fulltime", [
+        "This one of most Famous company in world. Many developer works many Country"
+      ]),
+      Message("Microsoft", "Native developer", "66/5 Bangladesh",
+          "images/m.22.png", "Fulltime", [
+        "This one of most Famous company in world. Many developer works many Country"
+      ]),
+      Message("Brain Station", "Web developer", "66/5 England",
+          "images/m.22.png", "Fulltime", [
+        "This one of most Famous company in world. Many developer works many Country"
+      ])
+    ];
   }
 }
